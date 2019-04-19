3221ac68472d09530b7ea6ca60f041be7ab0a28a
soohyeon13 <rlatngus6663@gmail.com>
2019-04-19 19:06:09 +0900

    13번(수정)

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -24,8 +24,8 @@ public class DaoFactory {
         return new UserDao(jdbcContext());
     }
     @Bean
-    public JdbcContext jdbcContext() {
-        return new JdbcContext(dataSource());
+    public JejuJdbcTemplate jdbcContext() {
+        return new JejuJdbcTemplate(dataSource());
     }
 
     @Bean
